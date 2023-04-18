5d2f1e1eb0f83f49d9cf6c06ae1c48644f112731
Quang Nguyen The <quangnguyenthe@Quangs-MacBook-Air.local>
2023-04-18 11:40:38 +0700

    added trpc route for note

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { createTRPCRouter } from "./trpc";
 import { exampleRouter } from "./routers/example";
 import { topicRouter } from "./routers/topic";
+import { noteRouter } from "./routers/note";
 
 
 /**
@@ -11,6 +12,7 @@ import { topicRouter } from "./routers/topic";
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
   topic: topicRouter,
+  note: noteRouter
 });
 
 // export type definition of API
